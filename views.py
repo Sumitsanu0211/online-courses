e93566ef4af0f10a0e38fe296c652f1d9d0ae33d
Sumit Kumar <101593839+Sumitsanu0211@users.noreply.github.com>
2023-07-26 17:28:43 +0530

    Update views.py

--- views.py
+++ views.py
@@ -10,3 +10,30 @@
     {% endfor %}
     <input type="submit" value="Submit">
 </form>
+
+
+from django.shortcuts import render, get_object_or_404
+from .models import Submission
+
+def evaluate_submission(request, submission_id):
+    submission = get_object_or_404(Submission, pk=submission_id)
+    
+    # Perform evaluation logic here and set the score based on the answers in the submission
+    # For example:
+    # score = 0
+    # for answer in submission.answers.all():
+    #     if answer.choice.is_correct:
+    #         score += 1
+    
+    # You can then update the submission model with the calculated score, if needed.
+    # For example:
+    # submission.score = score
+    # submission.save()
+    
+    # Prepare data to pass to the evaluation_result template
+    context = {
+        'submission': submission,
+        # Include any other data you want to display in the evaluation result template
+    }
+    
+    return render(request, 'evaluation_result.html', context)
